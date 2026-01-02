058fb465f515584099254e0d228569d02018798d
ra-mb-13 <raquelmoraisbelo@pm.me>
2026-01-02 01:10:28 +0000

    updates

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
@@ -51,6 +52,46 @@ module.exports = function (eleventyConfig) {
     }
 
     return content;
+  });
+
+  eleventyConfig.addShortcode("img", async function ({ src, alt, width, height, widths, className, imgDir, sizes = "100vw"}) {
+    if (alt === undefined) {
+      throw new Error(`Missing \`alt\` on responsive image from: ${src}`);
+    }
+
+    const IMAGE_DIR = imgDir || "./src/images/";
+    const metadata = await Image(IMAGE_DIR + src, {
+      widths: widths || [300, 480, 640, 1024],
+      formats: ["webp", "jpeg"],
+      urlPath: "/img/",
+      outputDir: "_site/img",
+      defaultAttributes: {
+        loading: "lazy",
+        decoding: "async"
+      }
+    });
+
+    let lowsrc = metadata.jpeg[0];
+    let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+
+    const sources = Object.values(metadata).map((imageFormat) => {
+      const srcType = imageFormat[0].sourceType;
+      const srcset = imageFormat.map(entry => entry.srcset).join(", ");
+      return `<source type="${srcType}" srcset="${srcset}" sizes="${sizes}">`
+    }).join("\n");
+
+    const img = `
+      <img
+        src="${lowsrc.url}"
+        width="${highsrc.width}"
+        height="${highsrc.height}"
+        alt="${alt}"
+        loading="lazy"
+        decoding="async"
+        class="${className || ''}"
+      >`;
+
+    return `<picture>\n\t${sources}\n\t${img}</picture>`;
   });
 
   // Let Eleventy transform HTML files as nunjucks
